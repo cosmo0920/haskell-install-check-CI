3c1f65e674afe2e2fd6c9e72b27a6b71c1608621
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-26 03:07:11 +0900

    fix date

--- yesod_daily_build_with_docker.sh
+++ yesod_daily_build_with_docker.sh
@@ -1,14 +1,15 @@
 export TWITTER_USER=kosmo__
-export DATE="`date +\"%Y%m%d%H%M%S\"`"
+export DATE="`date +\"%Y/%m/%d %H:%M:%S\"`"
 export CABAL_COMMAND="cabal install yesod yesod-bin"
-export BUILD_ENV="docker: 0.11.1 Ubuntu: 14.04 ghc: 7.6.3 cabal: 1.20 and haskell platform: 2013.2.0.0"
+export BUILD_ENV="docker: 0.11.1 Ubuntu: 14.04 and HP: 2013.2.0.0"
+export DOCKER_DATE="`date +\"%Y%m%d%H%M%S\"`"
 echo "=== \"${CABAL_COMMAND}\" with ${BUILD_ENV} scheduled. at ${DATE} ===" | tw --user=${TWITTER_USER} --pipe
 # prepare docker
 boot2docker up
 docker ps -a -q | xargs docker rm
 
 # build phase
-docker build -t yesodbox_"${DATE}" - < Dockerfile
+docker build -t yesodbox_"${DOCKER_DATE}" - < Dockerfile
 
 # tweet phase
 if [ $? == 0 ]; then
