eb281a96cf0d43f81094210bdad4415b2250ad8d
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-26 02:19:41 +0900

    fix setting

--- yesod_daily_build_with_docker.sh
+++ yesod_daily_build_with_docker.sh
@@ -1,6 +1,7 @@
+export TWITTER_USER=kosmo__
 export DATE="`date +\"%Y%m%d%H%M%S\"`"
 export CABAL_COMMAND="cabal install yesod yesod-bin"
-export BUILD_ENV="docker: 0.11.1 Ubuntu: 14.04 ghc: 7.6.3 cabal: 1.20"
+export BUILD_ENV="docker: 0.11.1 Ubuntu: 14.04 ghc: 7.6.3 cabal: 1.20 and haskell platform: 2013.2.0.0"
 echo "=== \"${CABAL_COMMAND}\" with ${BUILD_ENV} scheduled. at ${DATE} ===" | tw --user=${TWITTER_USER} --pipe
 # prepare docker
 boot2docker up
