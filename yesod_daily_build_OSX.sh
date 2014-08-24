51fbee00479cbe1a7ff5afc6b8ea3102980511e4
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-24 18:16:28 +0900

    use aeson-0.7.0.6

--- yesod_daily_build_OSX.sh
+++ yesod_daily_build_OSX.sh
@@ -1,5 +1,5 @@
 export TWITTER_USER=kosmo__
-export CABAL_COMMAND="cabal install yesod yesod-bin aeson-0.7.0.4"
+export CABAL_COMMAND="cabal install yesod yesod-bin aeson-0.7.0.6"
 export DATE="`date +\"%Y/%m/%d %H:%M:%S\"`"
 export BUILD_ENV="OSX: 10.8 ghc: 7.8.3 cabal: 1.20 and HP: 2014.2.0.0"
 echo "=== \"${CABAL_COMMAND}\" with ${BUILD_ENV} scheduled. at ${DATE} ===" | tw --user=${TWITTER_USER} --pipe
@@ -20,7 +20,7 @@ fi
 cabal update
 cabal sandbox init
 cabal install alex happy
-cabal install yesod yesod-bin aeson-0.7.0.4 # workaround for aeson
+cabal install yesod yesod-bin aeson-0.7.0.6 # workaround for aeson
 
 if [ $? == 0 ]; then
   echo "${BUILD_ENV} \"${CABAL_COMMAND}\" with ${BUILD_ENV} success! at ${DATE}" | tw --user=${TWITTER_USER} --pipe
