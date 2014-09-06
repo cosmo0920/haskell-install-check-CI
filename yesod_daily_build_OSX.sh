cec73eda0a47a4c7796ddb87c185836a47b4ebc9
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-09-06 15:19:15 +0900

    remove explicitly install `aeson-0.7.0.6`
    New yesod-auth has benn released!!

--- yesod_daily_build_OSX.sh
+++ yesod_daily_build_OSX.sh
@@ -1,5 +1,5 @@
 export TWITTER_USER=kosmo__
-export CABAL_COMMAND="cabal install yesod yesod-bin aeson-0.7.0.6"
+export CABAL_COMMAND="cabal install yesod yesod-bin"
 export DATE="`date +\"%Y/%m/%d %H:%M:%S\"`"
 export BUILD_ENV="OSX: 10.8 ghc: 7.6.3 cabal: 1.20 and HP: 2013.2.0.0"
 echo "=\"${CABAL_COMMAND}\" with ${BUILD_ENV} sched. at ${DATE}=" | tw --user=${TWITTER_USER} --pipe
@@ -20,7 +20,7 @@ fi
 cabal update
 cabal sandbox init
 cabal install alex happy
-cabal install yesod yesod-bin aeson-0.7.0.6 # workaround for aeson
+cabal install yesod yesod-bin
 
 if [ $? == 0 ]; then
   echo "${BUILD_ENV} \"${CABAL_COMMAND}\" success! at ${DATE}" | tw --user=${TWITTER_USER} --pipe
