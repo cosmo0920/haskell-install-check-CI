901257bd8f38444e3ca16ae6ea55c94972c8d556
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-09-06 17:02:17 +0900

    modify BUILD_ENV

--- yesod_daily_build_with_docker.sh
+++ yesod_daily_build_with_docker.sh
@@ -1,6 +1,6 @@
 export TWITTER_USER=kosmo__
 export DATE="`date +\"%Y/%m/%d %H:%M:%S\"`"
-export CABAL_COMMAND="cabal install yesod yesod-bin"
+export CABAL_COMMAND="cabal install yesod yesod-bin aeson-0.7.0.6"
 export BUILD_ENV="docker: 1.2.0 Ubuntu: 14.04 ghc: 7.8.3"
 export DOCKER_DATE="`date +\"%Y%m%d%H%M%S\"`"
 echo "=\"${CABAL_COMMAND}\" with ${BUILD_ENV} sched. at ${DATE}=" | tw --user=${TWITTER_USER} --pipe
